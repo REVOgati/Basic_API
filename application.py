a39a501e1cb3dc523273c441631e98ef3ea0cd9e
Kimutai101 <tiropgarreth@gmail.com>
2024-10-29 11:59:08 +0300

    Implemented GET, POST and DELETE, HTTP Methods

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__) #Creating instance of the Flask class
 
@@ -22,7 +22,40 @@ def index(): # What we want to happen when the route is hit
 #Create an app to get drinks(get request)
 @app.route('/drinks')
 def get_drinks():
-    return {'drinks': 'Grayson'}
+    drinks = Drink.query.all()
+    
+    output = []
+    for drink in drinks:
+        drink_data = {"name" : drink.name, "description" : drink.description}
+        output.append(drink_data)
+    
+    return {"drinks": output}
 
-#Connecting to a database
-#We will define all things we want to store in our database as modules (line 4)
+@app.route('/drinks/<id>')
+def get_drink(id):
+    drink =Drink.query.get_or_404(id)
+    return {'name': drink.name, 'description': drink.description}
+
+@app.route('/drinks', methods = ['POST'])
+def add_drink():
+    drink = Drink(name = request.json['name'], description = request.json['description'])
+    db.session.add(drink)
+    db.session.commit()
+    
+    return {"id ": drink.id}
+        
+@app.route('/drinks/<id>', methods = ['DELETE'])
+def delete_drink(id):
+    drink = Drink.query.get(id)
+    if drink is None:
+        return "Error: There is no drink of that id in the database"
+    else:
+        deleted_drink = drink.name
+        db.session.delete(drink)
+        db.session.commit()
+        return "The drink with name " + deleted_drink + " has been deleted"
+    
+    
+        
+        
+        
